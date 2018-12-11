9edd3f85293c8c8c9851863f8cdaa9f5c7ec3559
Ramazan18 <ramzik18@inbox.ru>
2018-12-11 12:04:24 +0600

    Initial commit

---
+++ app/src/main/java/contacts/ramazan/kz/contacts/ui/contacts/ContactClickCallback.java
@@ -0,0 +1,7 @@
+package contacts.ramazan.kz.contacts.ui.contacts;
+
+import contacts.ramazan.kz.contacts.data.Contact;
+
+public interface ContactClickCallback {
+    void onClick(Contact contact);
+}
---
+++ app/src/main/java/contacts/ramazan/kz/contacts/utils/Constants.java
@@ -0,0 +1,5 @@
+package contacts.ramazan.kz.contacts.utils;
+
+public class Constants {
+    public static final String CONTACTSFRAGMENT = "ContactsFragment";
+}
